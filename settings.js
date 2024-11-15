188a6fa279b4125a108b1d21bd5e13b09626402b
Ivan Zatevakhin <zatevaxin.ivan@gmail.com>
2024-11-15 00:20:04 +0000

    feat: added configurable flow file

--- settings.js
+++ settings.js
@@ -32,7 +32,7 @@ module.exports = {
  ******************************************************************************/
 
     /** The file containing the flows. If not set, defaults to flows_<hostname>.json **/
-    flowFile: 'flows.json',
+    flowFile: process.env.NODE_RED_DEFAULT_FLOW_FILE || "flow.json",
 
     /** By default, credentials are encrypted in storage using a generated key. To
      * specify your own secret, set the following property.
